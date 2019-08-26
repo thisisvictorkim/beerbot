c3f1773b3418bbb9f0a71df04cdd7585658d7441
thisisvictorkim <thisisvictorkim@gmail.com>
2019-08-26 15:45:34 -0400

    utc

--- app.js
+++ app.js
@@ -10,19 +10,21 @@ const app = new App ({
 
 var cTime = new moment();
 var dTime = new moment().hour(16).minute(30).second(0).millisecond(0);
-var testTime = new moment();
-testTime.local();
+
 var early = dTime.diff(cTime, 'minutes');
 var late = cTime.diff(dTime, 'minutes');
 
 //beer bot with momentJS
 //listen to incoming messages
 app.message(':beer:', ({message, say}) => {
+  var testTime = new moment();
+  testTime.local();
   if (cTime >= dTime) {
     say (`What are you waiting for <@${message.user}>? You could've been drinking for ${late} minutes already!`);
   } else {
     say (`Pump the breaks there <@${message.user}>... drink o'clock is ${early} minutes away`);
   }
+  console.log(testTime);
 });
 
 (async () => {
@@ -30,6 +32,5 @@ app.message(':beer:', ({message, say}) => {
   await app.start(process.env.PORT || 5000);
   console.log('⚡️ Bolt app is running!;');
   console.log(cTime);
-  console.log(testTime);
   console.log(dTime);
 })();
